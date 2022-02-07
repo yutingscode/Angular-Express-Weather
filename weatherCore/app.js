c5652eb174a841773344c33a9e38a232401336ec
yutingscode <w.yutingwang@gmail.com>
2022-02-07 13:25:17 -0500

    init

---
+++ weatherCore/app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const https = require('https')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+var cors = require('cors');
+app.use(cors({origin: 'http://localhost:4200'}));
+
+app.post('/', (req, res) => {
+  const name = req.body.name;
+
+  https.get('https://api.openweathermap.org/data/2.5/weather?q='+name+',,CA&appid=APPID', (resp) => {
+    
+    let data = '';
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    resp.on('end', () => {
+      console.log(JSON.parse(data));
+      res.status(200).json(JSON.parse(data));
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+
+
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
